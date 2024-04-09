5cea28525b108221d36733414c14f7b02e3a1e18
Ranganna-gift <82564568+Ranganna-gift@users.noreply.github.com>
2024-04-09 23:47:10 +0530

    Frontendimplemetation

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import GridComponet from './GridComponet';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Product Management System</h1>
+      <GridComponet />
+    </div>
+  );
+}
+
+export default App;
